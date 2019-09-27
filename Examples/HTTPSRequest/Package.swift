91261c553304d47cb9c40e5082993c9f1dd51976
Andrea Scuderi <andreascuderi@ymail.com>
2019-09-27 12:13:37 +0100

    Update release to 1.0.0-alpha.2

--- Examples/HTTPSRequest/Package.swift
+++ Examples/HTTPSRequest/Package.swift
@@ -8,8 +8,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         //.package(path: "../../../aws-lambda-swift-sprinter-nio-plugin"),
-        //.package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-nio-plugin", from: "1.0.0-alpha.1")
-        .package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-nio-plugin", .branch("feature/swift-5.1")),
+        .package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-nio-plugin", from: "1.0.0-alpha.2"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
--- Examples/S3Test/Package.swift
+++ Examples/S3Test/Package.swift
@@ -8,8 +8,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         //.package(path: "../../../aws-lambda-swift-sprinter-core"),
-        //.package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-core", from: "1.0.0-alpha.1")
-        .package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-core", .branch("feature/swift-5.1")),
+        .package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-core", from: "1.0.0-alpha.2"),
         .package(url: "https://github.com/Andrea-Scuderi/aws-sdk-swift.git", .branch("nio2.0-swift5.1")),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
