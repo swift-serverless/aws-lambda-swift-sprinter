2e575768792c0728fb386dc4e6b33f224526274c
Andrea Scuderi <andreascuderi@ymail.com>
2020-05-03 14:28:11 +0100

    Update S3Test with aws-sdk-swift 5.0.0-alpha.3

--- Examples/S3Test/Package.swift
+++ Examples/S3Test/Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "S3Test",
+    products: [
+        .executable(name: "S3Test", targets: ["S3Test"])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -11,7 +14,7 @@ let package = Package(
         //.package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-core", from: "1.0.0-alpha.3"),
         //.package(path: "../../../aws-lambda-swift-sprinter-nio-plugin"),
         .package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-nio-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", from: "4.0.0"),
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", from: "5.0.0-alpha.3"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
@@ -19,7 +22,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "S3Test",
-            dependencies: ["LambdaSwiftSprinterNioPlugin", "S3", "Logging"]
+            dependencies: [
+                "LambdaSwiftSprinterNioPlugin",
+                .product(name: "AWSS3", package: "aws-sdk-swift"),
+                "Logging"
+            ]
         ),
         .testTarget(
             name: "S3TestTests",
